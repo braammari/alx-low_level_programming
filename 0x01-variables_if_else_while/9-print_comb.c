d4be2d9597205507d8d4819cee5869fb8abc1717
brahim aammari <brahimaa.494@gmail.com>
2023-07-14 16:55:15 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include <time.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i <= 8)
+		{
+			putchar(i + '0');
+			putchar(44);
+			putchar(32);
+		}
+		else
+		{
+			putchar(i + '0');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
