8040970c9755bcbfc364671e4899b1f520ddbfa9
brahim aammari <brahimaa.494@gmail.com>
2023-07-14 12:47:58 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include <time.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' &&  ch != 'q')
+		{
+			putchar(ch);
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
