4c367548e6648ed6b2813d8ceb96326e6f1d905f
brahim aammari <brahimaa.494@gmail.com>
2023-07-20 16:07:01 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - prints _ n times.
+ * Return: Alays 0.
+ * @n: is an integer
+ **/
+
+void print_line(int n)
+{
+int i;
+for (i = 0; i < n ; i++)
+{
+	_putchar(95);
+}
+_putchar('\n');
+}
