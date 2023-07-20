7dfcfb73eee7c0bd97684221ddb23688d004dc18
brahim aammari <brahimaa.494@gmail.com>
2023-07-20 16:38:32 -0700

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints _ n times.
+ * Return: Alays 0.
+ * @n: is an integer
+ **/
+void print_diagonal(int n)
+{
+int i, j;
+for (i = 0; i < n ; i++)
+{
+	for (j = 0; j < i ;j++)
+	{
+        _putchar(32);
+	}
+	_putchar(92);
+	_putchar('\n');
+}
+_putchar('\n');
+}
