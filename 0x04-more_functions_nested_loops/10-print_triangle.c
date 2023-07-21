a20db2eb6bd7257367787940827bcf614edab1f3
brahim aammari <brahimaa.494@gmail.com>
2023-07-21 05:15:37 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints _ n times.
+ * Return: Alays 0.
+ * @size: is an integer
+ **/
+
+void print_triangle(int size)
+{
+int i, j, k;
+for (i = 0; i < size ; i++)
+{
+	for (j = size ; j > i ; j--)
+	{
+		_putchar(32);
+
+	}
+	for (k = 0; k <= i ; k++)
+        {
+                _putchar(35);
+        }
+	_putchar('\n');
+	if (i != size - 1)
+	{
+		_putchar('\n');
+	}
+}
+
+}
