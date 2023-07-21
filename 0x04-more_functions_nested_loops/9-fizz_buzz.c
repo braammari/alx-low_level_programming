dc177039743225512dce79a7f85fd7ec82bfa485
brahim aammari <brahimaa.494@gmail.com>
2023-07-20 17:43:13 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,11 +15,11 @@ for (i = 1; i < 101; i++)
 {
 	if (i % 3 ==  0)
 	{
-		printf("Fizz");
+		printf("Fizz ");
 	}
 	else if (i % 5 == 0)
 	{
-		printf("Buzz");
+		printf("Buzz ");
 	}
 	else if ((i % 3 ==  0) && (i % 5 ==  0))
 	{
