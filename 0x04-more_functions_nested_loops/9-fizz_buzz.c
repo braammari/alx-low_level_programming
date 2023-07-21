529a269b174d97e0c6793a2d87007dff09c81887
brahim aammari <brahimaa.494@gmail.com>
2023-07-20 17:41:35 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include <time.h>
+
+/**
+ * main - checks the code.
+ * Return: Alays 0.
+ **/
+
+int main(void)
+{
+
+int i;
+for (i = 1; i < 101; i++)
+{
+	if (i % 3 ==  0)
+	{
+		printf("Fizz");
+	}
+	else if (i % 5 == 0)
+	{
+		printf("Buzz");
+	}
+	else if ((i % 3 ==  0) && (i % 5 ==  0))
+	{
+		printf("FizzBuzz");
+	}
+	else
+	{
+		printf("%d ", i);
+	}
+}
+printf("\n");
+return (0);
+}
