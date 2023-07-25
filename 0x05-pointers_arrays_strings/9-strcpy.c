5ef6b30f333863bf561f3b425cb3c79c1c255e6d
brahim aammari <brahimaa.494@gmail.com>
2023-07-24 18:30:33 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *_strcpy - gives the lenght of a string
+ *@dest: is an array
+ *@src: is integer
+ * Return: always 0.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+int i;
+int n = 0;
+char t[1000];
+dest = t;
+
+while (src[n] != '\0')
+{
+n++;
+}
+for (i = 0; i < n; i++)
+{
+dest[i] = src[i];
+}
+return (dest);
+}
