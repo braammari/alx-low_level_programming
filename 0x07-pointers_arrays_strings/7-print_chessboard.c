694a66c0ae6fdc3fbcdc96cce86fa322ee886340
brahim aammari <brahimaa.494@gmail.com>
2023-08-01 16:35:44 -0700

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_chessboard - copy a string
+ * @a: is an array
+ * Return: 0
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			printf("%c", a[i][j]);
+		}
+		if (i < 7)
+		{
+			printf("\n");
+		}
+	}
+}
