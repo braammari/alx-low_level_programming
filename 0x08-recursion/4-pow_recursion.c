363cdcd51df3b400ed790485e9810dc97e5b7bbc
brahim aammari <brahimaa.494@gmail.com>
2023-08-02 16:17:33 -0700

    3-factorial.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - print string
+ * @x: is an array
+ *  @y: is an array
+ * Return: i
+*/
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	if (y == 1)
+	{
+		return (x);
+	}
+return (x * _pow_recursion(x, y - 1));
+}
