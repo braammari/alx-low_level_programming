971c677f3e9bc0587d0955cb175bbe0ff066d1bd
brahim aammari <brahimaa.494@gmail.com>
2023-08-01 18:06:48 -0700

    8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_diagsums - copy a string
+ * @a: is an array
+ * @size: is an integer
+ * Return: 0
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, s1 = 0, s2 = 0, s = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		s1 = s1 + a[i * size + i];
+		s2 = s2 + a[(i + 1) * size - i - 1];
+	}
+printf("%d,  %d\n", s1, s2);
+}
