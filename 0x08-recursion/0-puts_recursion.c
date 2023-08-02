ab3d6a12fcfea79ee628698491e2a429569d3b8e
brahim aammari <brahimaa.494@gmail.com>
2023-08-02 11:40:39 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - print string
+ * @s: is an array
+ * Return: s
+ */
+
+void _puts_recursion(char *s)
+{
+	char *str;
+
+	str = s;
+	if (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+		_puts_recursion(str);
+	}
+	else
+	{
+		return;
+	}
+}
