938feaff5ffca84a75abad4e310b8467b588a453
brahim aammari <brahimaa.494@gmail.com>
2023-08-05 15:22:41 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * @argc: is integer count
+ * @argv: is an array
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, p = 1;
+
+	for (i = 1; i < argc; i++)
+	{
+		p = p * atoi(argv[i]);
+	}
+printf("%d\n", p);
+return (0);
+}
