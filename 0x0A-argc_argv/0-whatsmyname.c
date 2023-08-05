52a16815df44e5bc83ca0961c14df12637a9348e
brahim aammari <brahimaa.494@gmail.com>
2023-08-05 13:58:03 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * @argc: is integer count
+ * @argv is an array
+ * Return: Always 0 (Success)
+ */
+
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+
+	printf("the name of program : %s\n", argv[0]);
+	return (0);
+}
