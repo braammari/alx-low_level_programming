15ec918c274dfea1fbc5bcb9467180f626e7f5d2
brahim aammari <brahimaa.494@gmail.com>
2023-08-05 14:07:26 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * @argc: is integer count
+ * @argv: is an array
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char __attribute__((unused)) argv[])
+{
+	printf("%s\n", argc);
+	return (0);
+}
