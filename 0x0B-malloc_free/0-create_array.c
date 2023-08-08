8052dc899796c089661ced18411c79bc9438bfea
brahim aammari <brahimaa.494@gmail.com>
2023-08-08 16:55:41 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * *create_array - copy a string
+ * @c: is an array
+ * @size: is an integer
+ * Return: 0
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	char *p = malloc(size * sizeof(char));
+
+	if (*p == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		*(p + i) = c;
+	}
+	return (p);
+}
+
