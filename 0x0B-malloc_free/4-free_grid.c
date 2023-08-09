1161f616df2cf5d72c3c5e128519e4d2c36592e2
brahim aammari <brahimaa.494@gmail.com>
2023-08-09 16:38:22 -0700

    4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -11,9 +11,13 @@ void free_grid(int **grid, int height)
 {
 	int i;
 
+	if (grid == NULL)
+	{
+		return;
+	}
 	for (i = 0; i < height; i++)
 	{
-		free (grid[i]);
+		free(grid[i]);
 	}
-	free (grid);
+	free(grid);
 }
