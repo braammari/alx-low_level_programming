6a196251d3258251daa5f7330e2192d2bf26b007
brahim aammari <brahimaa.494@gmail.com>
2023-08-14 11:28:08 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * struct dog - Short description
+ * @name: first basic type
+ * @age: Second basic type
+ *@owner: tirst basic type
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* MAIN_H */
