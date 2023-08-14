fb8b153bf7f78d822a9498425d42ffee3959833b
brahim aammari <brahimaa.494@gmail.com>
2023-08-14 12:42:35 -0700

    2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+
+/**
+ * print_dog - initialize struct dog
+ * @d: First member
+ * Description: Longer description
+ */
+
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	printf("Name: %s\n", (*d).name);
+	printf("Age: %f\n", (*d).age);
+	printf("Owner: %s\n", (*d).owner);
+}
