dc8c92404e580b56f144b90f564a6f03f10829d3
brahim aammari <brahimaa.494@gmail.com>
2023-08-14 13:30:29 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,13 +11,12 @@
  *@owner: tirst basic type
  * Description: Longer description
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
