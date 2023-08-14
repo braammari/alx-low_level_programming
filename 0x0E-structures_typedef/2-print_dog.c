aac45ee21b70fd9e38a02b6faf7a557aae4fbc6e
brahim aammari <brahimaa.494@gmail.com>
2023-08-14 12:51:21 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,6 +12,10 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
+	if ((*d).owner == NULL)
+		return (0);
+	if ((*d).name == NULL)
+		return (0);
 	printf("Name: %s\n", (*d).name);
 	printf("Age: %f\n", (*d).age);
 	printf("Owner: %s\n", (*d).owner);
