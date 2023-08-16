519760059930808eea60ae894aefcb04c296bbcc
brahim aammari <brahimaa.494@gmail.com>
2023-08-16 12:27:37 -0700

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include "function_pointers.h"
+
+/**
+* int_index -  Function do things
+* @array : 1 param
+* @cmp:  param pointer to function
+* @size: 3 param
+*  Return: Always 0 (Success)
+*/
+
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	if (size <= 0)
+		return (-1);
+	if (array && cmp)
+	{
+		int i = 0;
+
+		while (cmp(array[i]) == 0)
+		{
+			i++;
+		}
+		if (i < size)
+			return (i);
+		else
+			return (-1);
+	}
+}
