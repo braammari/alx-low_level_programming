6b304cdf40263fa6126899a5d289317be3a44d1c
brahim aammari <brahimaa.494@gmail.com>
2023-08-16 11:14:35 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+* print_name -  Function do things
+* @name : 1 param
+* @f: 2 param pointer to function
+*  Return: Always 0 (Success)
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
