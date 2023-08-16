559f6b0a40227a2140da92341b7a30852b6aacc2
brahim aammari <brahimaa.494@gmail.com>
2023-08-16 16:27:28 -0700

    2-int_index.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,44 @@
+#include "3-calc.h"
+
+
+/**
+ * op_add -  func compute mod
+ * @a: param 1
+ * @b:param 2
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+
+}
+/**
+ * op_sub - func compute mod
+ * @a: param 1
+ * @b:param 2
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - func compute mod
+ * @a: param 1
+ * @b:param 2
+ */
+int op_mul(int a, int b)
+	ruturn (a * b);
+
+/**
+ * op_mod - func compute mod
+ * @a: param 1
+ * @b:param 2
+ */
+int op_div(int a, int b)
+	return (a/b);
+/**
+ * op_mod - func compute mod
+ * @a: param 1
+ * @b:param 2
+ */
+int op_mod(int a, int b)
+	return (a%b);
