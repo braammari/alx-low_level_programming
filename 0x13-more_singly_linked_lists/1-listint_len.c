5bc28c05c000e51e0e3c39060293750bb10d1e2a
brahim aammari <brahimaa.494@gmail.com>
2023-08-28 11:17:18 -0700

    ask1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,26 +3,17 @@
 #include "lists.h"
 
 /**
-* print_list -  Function do things
+* print_listint -  Function do things
 * @h: a pointer of type list_t
 * Return: i (Success)
 */
 
-
-size_t print_list(const list_t *h)
+size_t print_listint(const listint_t *h)
 {
 	size_t i = 0;
 
 	while (h != NULL)
 	{
-		if (h->str == NULL)
-		{
-			printf("[0] (nil)\n");
-		}
-		else
-		{
-			printf("[%d] %s\n", h->len, h->str);
-		}
 		i++;
 		h = h->next;
 	}
