97777b5845e3350e30cac4402cbda7f9a798f0cb
brahim aammari <brahimaa.494@gmail.com>
2023-08-28 11:13:37 -0700

    task0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,19 +3,18 @@
 #include "lists.h"
 
 /**
-* print_list -  Function do things
+* print_listint -  Function do things
 * @h: a pointer of type list_t
 * Return: i (Success)
 */
 
-
-size_t print_list(const list_t *h)
+size_t print_listint(const listint_t *h)
 {
 	size_t i = 0;
 
 	while (h != NULL)
 	{
-		printf("%d\n", h->n)
+		printf("%d\n", h->n);
 		i++;
 		h = h->next;
 	}
