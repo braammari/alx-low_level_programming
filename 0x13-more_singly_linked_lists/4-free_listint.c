35a9c6550dad4b654268a1e0402904152588fd8e
brahim aammari <brahimaa.494@gmail.com>
2023-08-28 17:13:51 -0700

    task4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -3,28 +3,18 @@
 #include "lists.h"
 
 /**
-* print_list -  Function do things
-* @h: a pointer of type list_t
-* Return: i (Success)
+* free_listint -  Function do things
+* @head: a pointer of type list_t
+* Return:  (Success)
 */
 
-
-size_t print_list(const list_t *h)
+void free_listint(listint_t *head)
 {
-	size_t i = 0;
-
-	while (h != NULL)
+	while (head != NULL)
 	{
-		if (h->str == NULL)
-		{
-			printf("[0] (nil)\n");
-		}
-		else
-		{
-			printf("[%d] %s\n", h->len, h->str);
-		}
-		i++;
-		h = h->next;
+		listint_t *aux = head;
+
+		head = head->next;
+		free(aux);
 	}
-	return (i);
 }
