fbdd2f13ec13554f135e8d4c43f875302fd32881
brahim aammari <brahimaa.494@gmail.com>
2023-08-29 16:45:04 -0700

    task9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,8 +20,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
-	if (*head == NULL || idx == 0)
+	if (*head == NULL)
 		return (NULL);
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+	}
 	while (aux != NULL && i < idx - 1)
 	{
 		i++;
